6a035b5492387cce590406462de2542205b7fd2f
Elian Maineri - BIT S.A <elianmaineri@gmail.com>
2023-06-19 18:06:20 -0300

    guardado controlador

--- Controller/Controlador.py
+++ Controller/Controlador.py
@@ -1,7 +1,17 @@
 from Views.Vista import Vista
+from Models.Cliente import Cliente
 
 class controlador:
-
+    def __init__(self, file, vista):
+            try:
+                self.cliente = []
+                self.Vista = vista
+                with open(file) as archivo:
+                    for line in archivo:
+                        data = line.strip().split(';')
+                        self.cliente.append(Cliente(str(data[0]), int(data[1]), int(data[2]), str(data[3]), str(data[4])))
+            except FileNotFoundError:
+                print('No hay datos cargados')
 
 
 
