656399386ad7a3538f66f255a2a288868f758a6f
Elian Maineri - BIT S.A <elianmaineri@gmail.com>
2023-06-19 17:45:40 -0300

    clase reserva

--- Models/Reserva.py
+++ Models/Reserva.py
@@ -1,14 +1,18 @@
 class Reserva:
     def __init__(self, dia, horaInicio, horaFin, seña):
         self.dia = dia
-        self.horaInicio = hora
+        self.horaInicio = horaInicio
+        self.horaFin = horaFin
         self.seña = seña
 
     def get_dia(self):
         return self.dia
     
-    def get_hora(self):
-        return self.hora
+    def get_horaInicio(self):
+        return self.horaInicio
+    
+    def get_horaFin(self):
+        return self.horaFin
     
     def get_seña(self):
         return self.seña
@@ -16,8 +20,11 @@ class Reserva:
     def set_dia(self, nuevo_dia):
         self.dia = nuevo_dia
 
-    def set_hora(self, nueva_hora):
-        self.hora = nueva_hora
+    def set_horaInicio(self, nueva_horaInicio):
+        self.horaInicio = nueva_horaInicio
+
+    def set_horaFin(self, nueva_horaFin):
+        self.horaFin = nueva_horaFin
 
     def set_seña(self, nueva_seña):
         self.seña = nueva_seña
