49d9a4b27fd341fc6c413c22bbd57412fdfda3e5
AgusPucci <pucciagustin99@gmail.com>
2023-06-19 18:29:52 -0300

    1.1

--- Controller/Controlador.py
+++ Controller/Controlador.py
@@ -2,22 +2,21 @@ from Views.Vista import Vista
 from Models.Cliente import Cliente
 
 class controlador:
-<<<<<<< HEAD
+    def __init__(self, vista):
+        self.vista = vista
     
-
-=======
-    def __init__(self, file, vista):
-            try:
-                self.cliente = []
-                self.Vista = vista
-                with open(file) as archivo:
-                    for line in archivo:
-                        data = line.strip().split(';')
-                        self.cliente.append(Cliente(str(data[0]), int(data[1]), int(data[2]), str(data[3]), str(data[4])))
-            except FileNotFoundError:
+    def cliente(self,file):   
+        self.cliente=[]
+        try:
+            with open(file) as archivo:
+                for line in archivo:
+                    data = line.strip().split(';')
+                    self.cliente.append(Cliente(str(data[0]), int(data[1]), int(data[2]), str(data[3]), str(data[4])))
+        except FileNotFoundError:
                 print('No hay datos cargados')
->>>>>>> 6a035b5492387cce590406462de2542205b7fd2f
 
+def fecha_dis(self):
+    
 
 
 
